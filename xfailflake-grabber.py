4799ef4ad93293b855509a1f4326c4ad5185510e
Alexander Rukletsov <alexr@apache.org>
2018-12-07 14:33:22 +0100

    Update the script to be compatible with both python 2 and 3.

--- xfailflake-grabber.py
+++ xfailflake-grabber.py
@@ -1,4 +1,13 @@
-import BaseHTTPServer, json, os, re, shutil, sys, time
+import codecs, json, os, re, shutil, sys, time
+
+# Dances around python versions and modules conflict
+try:
+    from BaseHTTPServer import HTTPServer
+    from BaseHTTPServer import BaseHTTPRequestHandler
+except ImportError:
+    from http.server import HTTPServer
+    from http.server import BaseHTTPRequestHandler
+
 
 dcos_oss_dir = "dcos"
 dcos_oss_repo = "https://github.com/dcos/dcos.git"
@@ -15,6 +24,7 @@ def get_target_files(rootdir):
             target_files.append(os.path.join(folder, filename))
     return target_files
 
+
 # Scan contents of each target file in search for a specific pattern:
 #     `...xfailflake(reason="DCOS... .... (\n)*... def test_foo(...`
 # Once the pattern is matched extract DCOS or DCOS_OSS ticket and the associated
@@ -27,19 +37,20 @@ def get_xfailflakes_from_files(target_files):
     pattern = "xfailflake\(reason=\"(DCOS\S*).*?def\s*(\S*)\("
     xfailflakes = []
     for file in target_files:
-        with open(file, 'r') as f:
+        with codecs.open(file, "r", encoding='utf-8', errors='ignore') as f:
             contents = f.read()
             matched = re.findall(pattern, contents, re.MULTILINE|re.DOTALL)
             for t in matched:
                 if not t:
-                    print "Unexpected match in file \"{}\"".format(file)
+                    print("Unexpected match in file \"{}\"".format(file))
                 if len(t) != 2:
-                    print "Match {} in file \"{}\" has {} components while 2 are expected".format(t, file, len(t))
+                    print("Match {} in file \"{}\" has {} components while 2 are expected".format(t, file, len(t)))
                 xfailflakes.append({
                     "file": file,
                     "test": t[1],
                     "ticket": t[0]})
     return xfailflakes
+
 
 # Combines all pieces together: clone the repo, scan it, generate the output.
 def get_xfailflakes_from_repo(repo, tmpdir):
@@ -50,10 +61,11 @@ def get_xfailflakes_from_repo(repo, tmpdir):
     xfailflakes = get_xfailflakes_from_files(target_files)
 
     # Cleanup after ourselves.
-    print "Cleaning up: removing {}".format(tmpdir)
+    print("Cleaning up: removing {}".format(tmpdir))
     shutil.rmtree(tmpdir)
 
     return xfailflakes
+
 
 # Bakes xfailflakes together with a timestamp and a repo, and spits out JSON.
 def convert_to_default_format(xfailflakes, repo):
@@ -65,7 +77,9 @@ def convert_to_default_format(xfailflakes, repo):
 
     return json.dumps(output)
 
-# Redash requires particular format for JSON input, see https://redash.io/help/data-sources/querying-urls
+
+# Redash requires particular format for JSON input,
+# see https://redash.io/help/data-sources/querying-urls .
 # We organise the output in three columns: "test", "ticket", "file".
 def convert_to_redash(xfailflakes):
     columns = [
@@ -95,8 +109,9 @@ def convert_to_redash(xfailflakes):
 
     return json.dumps(output)
 
+
 # For each GET request this handler replies with JSON in redash format.
-class RedashHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class RedashHandler(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
@@ -107,12 +122,13 @@ class RedashHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         dcos_oss_output = get_xfailflakes_from_repo(dcos_oss_repo, dcos_oss_dir)
         self.wfile.write(convert_to_redash(dcos_oss_output))
 
+
 # Starts a simple server with the `RedashHandler`.
 def serve(port):
     server_address = ('', port)
-    httpd = BaseHTTPServer.HTTPServer(server_address, RedashHandler)
+    httpd = HTTPServer(server_address, RedashHandler)
 
-    print "Starting httpd on port {}".format(port)
+    print("Starting httpd on port {}".format(port))
     httpd.serve_forever()
 
 
@@ -123,5 +139,5 @@ if __name__ == "__main__":
         serve(int(sys.argv[1]))
     else:
         dcos_oss_output = get_xfailflakes_from_repo(dcos_oss_repo, dcos_oss_dir)
-        print "xfailflakes JSONified: '{}'".format(convert_to_default_format(dcos_oss_output, dcos_oss_repo))
-        print "xfailflakes redashified: '{}'".format(convert_to_redash(dcos_oss_output))
+        print("xfailflakes JSONified: '{}'".format(convert_to_default_format(dcos_oss_output, dcos_oss_repo)))
+        print("xfailflakes redashified: '{}'".format(convert_to_redash(dcos_oss_output)))
